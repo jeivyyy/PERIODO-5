50b7e555598a317c44a5a8e9560698ce2e923935
2NingGiselleCachorra <rjoao@alunos.afya.com.br>
2026-03-30 18:14:44 -0400

    reajusted - web prog.

---
+++ WEB_PROGRAMMING/AULA 3/atv3.js
@@ -0,0 +1,14 @@
+// AULA DIA 02/03.
+
+/*Escreva um pequeno código em Javascript que :
+- Crie uma Queue
+- Insira três elementos
+- Remova e imprima o primeiro elemento*/
+
+let queue = []
+queue.push("\nJoão Vitor")
+queue.push("Domenique Joli")
+queue.push("Asaffe Ayron")
+
+console.log(`${queue}\n`)
+console.log(`Removido: ${queue.shift()}`)
